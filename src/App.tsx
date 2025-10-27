177855fb330da08b334c32580a5ce529bc5889fd
Lisa <hello@lisab.dev>
2025-10-26 21:35:43 -0700

    Add GPUTier checking

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Canvas, useFrame, type CameraProps } from "@react-three/fiber";
 import "./App.css";
 import Room from "./assets/Room";
 import { MathUtils, Vector3 } from "three";
+import { useDetectGPU } from "@react-three/drei";
 
 // const cameraPos = new Vector3(0, 1, 2);
 const cameraPos = new Vector3(0, 0, 2);
 const cameraProps: CameraProps = { fov: 50, position: cameraPos };
 // const cameraAngleX = cameraPos.angleTo(new Vector3(0, cameraPos.y, 0));
-const roomPos = new Vector3(0, -1.6, -2);
+const roomPos = new Vector3(0, -1.6, -2); // puts computers around world origin
 
 const CameraMovement = () => {
     useFrame((state) => {
@@ -20,6 +21,10 @@ const CameraMovement = () => {
 };
 
 const App = () => {
+    const GPUTier = useDetectGPU();
+
+    console.log(GPUTier);
+
     return (
         <>
             <Canvas
@@ -32,8 +37,7 @@ const App = () => {
                 <ambientLight intensity={0.7} />
 
                 <group position={roomPos}>
-                    {/* world origin will correspond to computer location */}
-
+                    v{" "}
                     <pointLight
                         intensity={50}
                         position={[0, 5, 6]}
@@ -41,7 +45,6 @@ const App = () => {
                         shadow-mapSize={[2048, 2048]}
                         shadow-radius={2}
                     />
-
                     <Room position={[0, 0, 0]} />
                 </group>
             </Canvas>
