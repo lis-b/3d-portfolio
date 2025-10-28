9427fced896a180c8992db7c85fc23bc58740f8a
Lisa <hello@lisab.dev>
2025-10-27 21:18:37 -0700

    Update model to remove monitors, replace with uikit using transformation matrices generated from obj files

--- src/App3D.tsx
+++ src/App3D.tsx
@@ -1,11 +1,14 @@
 import { Canvas, useFrame, type CameraProps } from "@react-three/fiber";
 import Room from "./assets/Room";
-import { MathUtils, Vector3 } from "three";
+import { MathUtils, Matrix4, Vector2, Vector3 } from "three";
+import { Container, Text } from "@react-three/uikit";
 
 const cameraPos = new Vector3(0, 0.75, 3.25);
 const cameraProps: CameraProps = { fov: 50, position: cameraPos };
 const initCameraAngleX = cameraPos.angleTo(new Vector3(0, 0, 1)); // required since the camera is angled towards the origin
 const roomPos = new Vector3(0, -1.6, -0.75); // puts computers around world origin
+
+const monitorSize = new Vector2(1.31, 0.751); // ever so slightly larger to fill in any gaps
 
 const CameraMovement = () => {
     useFrame((state) => {
@@ -19,6 +22,66 @@ const CameraMovement = () => {
     });
 
     return null;
+};
+
+const LeftMonitor = () => {
+    // generated transformation matrix from the objs extracted from blender
+    const transformation = new Matrix4().set(
+        0.99429392,
+        -0.02397525,
+        0.13597025,
+        -1.189941,
+        0.00000003,
+        0.98858372,
+        0.17431396,
+        1.654782,
+        -0.13806781,
+        -0.1726573,
+        0.97918837,
+        0.454409,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+    );
+
+    return (
+        <group matrix={transformation} matrixAutoUpdate={false}>
+            <Container backgroundColor="grey" sizeX={monitorSize.x} sizeY={monitorSize.y} flexDirection="row">
+                <Text>This is the left monitor.</Text>
+            </Container>
+        </group>
+    );
+};
+
+const RightMonitor = () => {
+    // generated transformation matrix from the objs extracted from blender
+    const transformation = new Matrix4().set(
+        1.00359112,
+        0.00383725,
+        -0.02176212,
+        0.38711075,
+        -0.0,
+        0.98858391,
+        0.1743138,
+        1.654782,
+        0.02209783,
+        -0.17427156,
+        0.98834435,
+        0.37031575,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+    );
+
+    return (
+        <group matrix={transformation} matrixAutoUpdate={false}>
+            <Container backgroundColor="grey" sizeX={monitorSize.x} sizeY={monitorSize.y} flexDirection="row">
+                <Text>This is the right monitor.</Text>
+            </Container>
+        </group>
+    );
 };
 
 const App3D = () => {
@@ -37,6 +100,8 @@ const App3D = () => {
                     shadow-radius={2}
                 />
                 <Room position={[0, 0, 0]} />
+                <LeftMonitor />
+                <RightMonitor />
             </group>
         </Canvas>
     );
